eb1229b2d73592f0b166f39d0b62d612463833a9
ThunderboltMonkey <fabian.castillo16@tectijuana.edu.mx>
2020-10-18 21:04:24 -0700

    Fixings

--- Practices/practice 1.R
+++ Practices/practice 1.R
@@ -18,39 +18,39 @@ Check that Mean(Xn)->E(X) as you rerun your script while increasing N
 
 
 > ## 1.  Initialize sample size
-```r
+~~~r
 x <- rnorm(10)
 y <- dnorm(x, mean = 0, sd = 1)
 plot(x,y)
-```
+~~~
 
 > ## 2. Initialize counter
 
-```r
+~~~r
 count <- 0
 res <- 0
-```
+~~~
 
 > ## 3.  Loop for(i in rnorm(size))
 
-```r
+~~~r
 for(i in rnorm(10))
 {
   count <- count + 1
   print(count)
-```
+~~~
 
 > ## 4. Check if the iterated variable falls between -1 and 1
 
-```r
+~~~r
 if(i <= 1 && i >= -1){
   print(i)
   print("It's between -1 y 1")
-```
+~~~
 
 > ## 5. Increase counter if the condition results true
 
-```r
+~~~r
   res <- res + 1
   }
   else
@@ -59,18 +59,18 @@ if(i <= 1 && i >= -1){
     print("Out of range")
   }
 }
-```
+~~~
 
 > ## 6. Return a result <- counter / N
 
-```r
+~~~r
   Res <- res / count
   print(Res)
-```
+~~~
 
 # **Full Script (the one running on R Studio)**
 
-```r
+~~~r
 x <- rnorm(10)
 y <- dnorm(x, mean = 0, sd = 1)
 plot(x,y)
@@ -96,4 +96,4 @@ for(i in rnorm(10))
 
 Res <- res / count
 print(Res)
-```
+~~~
