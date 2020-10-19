d413837add96e35458355513e50ce0eb1cca1917
ThunderboltMonkey <fabian.castillo16@tectijuana.edu.mx>
2020-10-18 21:50:20 -0700

    Fixings

--- Practices/practice 1.R
+++ Practices/practice 1.R
@@ -18,82 +18,68 @@ Check that Mean(Xn)->E(X) as you rerun your script while increasing N
 
 ## 1.  Initialize sample size
 
-~~~r
-x <- rnorm(10)
-y <- dnorm(x, mean = 0, sd = 1)
-plot(x,y)
-~~~
+    x <- rnorm(10)
+    y <- dnorm(x, mean = 0, sd = 1)
+    plot(x,y)
 
 ## 2. Initialize counter
 
-~~~r
-count <- 0
-res <- 0
-~~~
+    count <- 0
+    res <- 0
 
 ## 3.  Loop for(i in rnorm(size))
 
-~~~r
-for(i in rnorm(10))
-{
-  count <- count + 1
-  print(count)
-~~~
+    for(i in rnorm(10))
+    {
+      count <- count + 1
+      print(count)
 
 ## 4. Check if the iterated variable falls between -1 and 1
 
-~~~r
-if(i <= 1 && i >= -1){
-  print(i)
-  print("It's between -1 y 1")
-~~~
+    if(i <= 1 && i >= -1){
+      print(i)
+      print("It's between -1 y 1")
 
 ## 5. Increase counter if the condition results true
 
-~~~r
-  res <- res + 1
-  }
-  else
-  {
-    print(i)
-    print("Out of range")
-  }
-}
-~~~
+      res <- res + 1
+      }
+      else
+      {
+        print(i)
+        print("Out of range")
+      }
+    }
 
 ## 6. Return a result <- counter / N
 
-~~~r
-  Res <- res / count
-  print(Res)
-~~~
+    Res <- res / count
+    print(Res)
 
 # **Full Script (the one running on R Studio)**
 
-~~~r
-x <- rnorm(10)
-y <- dnorm(x, mean = 0, sd = 1)
-plot(x,y)
+    x <- rnorm(10)
+    y <- dnorm(x, mean = 0, sd = 1)
+    plot(x,y)
 
-count <- 0
-res <- 0
+    count <- 0
+    res <- 0
 
-for(i in rnorm(10))
-{
-  count <- count + 1
-  print(count)
-  if(i <= 1 && i >= -1){
-    print(i)
-    print("It's between -1 y 1")
-    res <- res + 1
-  }
-  else
-  {
-    print(i)
-    print("Out of range")
-  }
-}
+    for(i in rnorm(10))
+    {
+      count <- count + 1
+      print(count)
+      if(i <= 1 && i >= -1){
+        print(i)
+        print("It's between -1 y 1")
+        res <- res + 1
+      }
+      else
+      {
+        print(i)
+        print("Out of range")
+      }
+    }
 
-Res <- res / count
-print(Res)
-~~~
+    Res <- res / count
+    print(Res)
